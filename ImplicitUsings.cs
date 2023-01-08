39e2fafa07cd0e40a53bbeffdefa955d781696ad
AhmedNasser <60339928+AhmedNasser123@users.noreply.github.com>
2023-01-08 20:01:16 +0200

    Delete ImplicitUsings.cs

--- ImplicitUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using OpenQA.Selenium;
-global using OpenQA.Selenium.Support.UI;
-global using SeleniumExtras.PageObjects;
-global using TechTalk.SpecFlow;
-global using TechTalk.SpecFlow.Assist;
-global using FindsByAttribute = SeleniumExtras.PageObjects.FindsByAttribute;
-global using How = SeleniumExtras.PageObjects.How;
