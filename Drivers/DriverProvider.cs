901597ec46d1f0525a9d07cd535d05a4917a9800
AhmedNasser <an2621559@gmail.com>
2023-01-09 11:37:11 +0200

    Initial project

---
+++ Drivers/DriverProvider.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace Mazaady_task.Drivers
+{
+    [Binding]
+    public class DriverProvider //Driver
+    {
+        public static IWebDriver driver;
+        public data dt = new data();
+
+        public static IWebDriver GetDriver()
+        {
+            return InitializeDriver();
+        }
+
+        private static IWebDriver InitializeDriver()
+        {
+            data dt = new data();
+            if (driver == null)
+            {
+                var DeviceDriver = ChromeDriverService.CreateDefaultService();
+                DeviceDriver.HideCommandPromptWindow = true;
+                ChromeOptions options = new ChromeOptions();
+                options.AddExcludedArgument("enable-automation");
+                options.AddArgument("--disable-infobars");
+
+                options.AddArgument("no-sandbox");
+                options.AddArgument("single-process");
+                options.AddArgument("disable-dev-shm-usage");
+                options.AddArgument("incognito");
+                options.AddArgument("disable-blink-features=AutomationControlled");
+                options.AddAdditionalOption("useAutomationExtension", false);
+
+                options.AddUserProfilePreference("download.default_directory", dt.folderPath);
+                options.AddUserProfilePreference("intl.accept_languages", "nl");
+                options.AddUserProfilePreference("disable-popup-blocking", "true");
+
+                driver = new ChromeDriver(DeviceDriver, options);
+                /////////////////////////////////////////
+                ////driver = new FirefoxDriver();
+                //// driver = new EdgeDriver();
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://staging.mazaady.com/login");
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            }
+            return driver;
+        }
+
+        public static void CloseDriver()
+        {
+            if (driver != null)
+            {
+                driver.Close();
+                driver.Quit();
+            }
+        }
+
+
+    }
+}
---
+++ Feature/AddProuduct.feature
@@ -0,0 +1,24 @@
+﻿Feature: add product
+
+
+Scenario: User successfully logged in
+Given that an User on the login page
+When he enter the following data:
+| Email            | Password  |
+| test22@gmail.com | 123456789 |
+Then clicks on the login button
+Then Appear home page 
+
+#Scenario: From my profile  select Add product 
+Given  choose from menu of profile pic add product
+ 
+#Scenario: Fii the fields in Add prouduct form 
+ Given  Select single product
+ When  fill all element including(main image, auction details , policy) 
+ Then  choose selling type (estimation value) 
+ Then Click save 
+
+
+
+
+
